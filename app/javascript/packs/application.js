bde70ba1999b5012459a37578df8c2cd23689575
jun yamada <jacqk.rv@gmail.com>
2020-09-29 16:09:16 +0900

    2回目

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
+require("jquery/dist/jquery.js")
+require("bootstrap/dist/js/bootstrap.min")
 require("channels")
 
 
